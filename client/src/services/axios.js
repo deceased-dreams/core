93cd74793bbe5b5034fefde845c71366aa1ff3cf
jordan meta <irnawatyully@gmail.com>
2020-07-02 22:09:43 +0800

    client api url

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = process.env.NODE_ENV == 'development' 
+  ? 'http://localhost:3000'
+  : 'https://deceased-dreams.herokuapp.com';
 export default axios.create({
   baseURL: BASE_URL
 })
